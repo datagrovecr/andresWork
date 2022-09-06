7f7d642ddc30ff02e3235491d96db7e7701f3f3c
Andres Villalobos <andres.villalobos@datagrove.com>
2022-09-06 14:18:15 -0600

    Create Bootcamp.dart

---
+++ Bootcamp.dart
@@ -0,0 +1,21 @@
+import 'dart:web_gl';
+
+void main() {
+  print("This is my first sentence");
+}
+
+void secondary() {
+  var x = 5;
+  print(x);
+}
+
+//third function is about operators
+void third() {
+  print("Ross" + " " + "Cooper");
+}
+
+void four() {
+  num x = 1;
+  x--;
+  print(x);
+}
