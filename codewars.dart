283db730037be4f44aa71eaef4a0e8e7171aede1
Andres Villalobos <andres.villalobos@datagrove.com>
2022-09-06 14:33:37 -0600

    Update codewars.dart

--- codewars.dart
+++ codewars.dart
@@ -1,6 +1,6 @@
 // This function should test if the factor is a factor of base.
 //Return true if it is a factor or false if it is not.
-
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb/solutions/dart?filter=me&sort=best_practice&invalids=false
 bool checkForFactor(int base, int factor) {
   num check = base % factor;
   if (check == 0) {
