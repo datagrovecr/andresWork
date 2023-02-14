26c971267de293abfcfb814d088110b4d91cb936
Andres Villalobos <andres.villalobos@datagrove.com>
2023-02-14 11:40:57 -0600

    Create test2.py

---
+++ Coding/test2.py
@@ -0,0 +1,10 @@
+while True: 
+    print("Welcome to the menu. \n Choose an option \n 1.Close up \n 2.Show screen")
+    choice = int(input("choice?"))
+    if choice == 1:
+        print ("Byee!")
+        break
+    else:
+        print("This is a screen")
+        break
+    
