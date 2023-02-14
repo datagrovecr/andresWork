94300d2c9643f7152015771b5ead378e867507c2
Andres Villalobos <andres.villalobos@datagrove.com>
2023-02-14 11:46:32 -0600

    Update test2.py

--- Coding/test2.py
+++ Coding/test2.py
@@ -1,3 +1,4 @@
+
 while True: 
     print("Welcome to the menu. \n Choose an option \n 1.Close up \n 2.Show screen")
     choice = int(input("choice?"))
@@ -8,3 +9,10 @@ while True:
         print("This is a screen")
         break
     
+option:int = "10aaaaaaaaaaa"
+nombre:str = 'andres'
+is_man:bool = False
+board:list = ["_"] * 10
+
+
+print
