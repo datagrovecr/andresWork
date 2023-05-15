4ab4b83b9dc73324a86594ef0785677237c7950a
Andres Villalobos <andres.villalobos@datagrove.com>
2023-05-15 07:52:43 -0600

    learning go syntax

---
+++ Coding/learningGo.go
@@ -0,0 +1,18 @@
+package main
+
+//fmt Format package, allows input and output functionalities.
+import (
+	"fmt" 
+	//"math"
+)
+//import multiple packages
+
+func main(){
+	fmt.Println("Hello")
+	//variables
+	var i, a int
+	i, a = 8, 12
+	fmt.Println(i, a)
+	//short notation
+	//v := 8
+}
