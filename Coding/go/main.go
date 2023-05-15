e174d21dbb680a2637a941279d38cf786189a938
Andres Villalobos <andres.villalobos@datagrove.com>
2023-05-15 10:49:17 -0600

    swtch and conditionals

--- Coding/go/main.go
+++ Coding/go/main.go
@@ -26,3 +26,36 @@ func input(){
 	fmt.Scanln(&input)
 	fmt.Println(input)
 }
+func conditions(){
+	if x:=42; x>18 {
+		fmt.Println("Allowed")
+	}else {
+		fmt.Println("Not Allowed")
+	}
+
+	var fever float32
+    fmt.Scanln(&fever)
+    if fever >99.5{
+        fmt.Println("Fever")
+    }else {
+        fmt.Println("Allowed")
+    }
+}
+
+func switches(){
+	var f int
+	fmt.Scanln(&f)
+	//your code goes here
+	switch {
+	  case f < 0:
+		fmt.Println("Wrong Input")
+	  case f<20 :
+		fmt.Println("Infrasound")
+	  
+	  case f>20000:
+		fmt.Println("Ultrasound")
+	 default :
+		fmt.Println("Audible")
+	}
+}
+
